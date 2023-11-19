2b44912877dc2a761b0c2cce5386923d48bd1e05
Aliceonly <aliceonly@foxmail.com>
2023-11-19 17:18:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+yarn hardhat run scripts/deploy.js --network mumbai
+Contract Deployed at: 0x531f57DCBd640D2b5c51FCED647a41c7d9b7308d
+yarn dev
---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="relative">
+    </footer>
+  );
+};
+
+export default Footer;
